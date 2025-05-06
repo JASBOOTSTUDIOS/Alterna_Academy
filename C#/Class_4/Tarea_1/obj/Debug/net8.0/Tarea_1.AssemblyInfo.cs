274e04d8ef16548773cd9f9c9cc8025c057bd5ad
JAS BOOT STUDIOS <jasbootstudios@gmail.com>
2025-05-05 21:51:49 -0400

    add

--- C#/Class_4/Tarea_1/obj/Debug/net8.0/Tarea_1.AssemblyInfo.cs
+++ C#/Class_4/Tarea_1/obj/Debug/net8.0/Tarea_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tarea_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed92efc5040d1dfff0671e0f3d7b83ec48a537af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55e859d3ed91d7115c2ff4616a1a5a05ad4013da")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tarea_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tarea_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
